469c77e6dd9ef1b1ef8f5ff6c3e7c2670ac4a2ed
UsmanShafiq <usmanshafiq933@gmail.com>
2022-01-31 01:00:05 +0500

    Changes

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,65 @@
+import React, { Fragment } from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import { Container, ContentWrapper, LeftSection, RightSection, LoginForm } from './StyledLogin';
+
+
+
+export default function Login() {
+  const   LoginSchema = Yup.object().shape({
+    password: Yup.string().required('Password is Required.'),
+    email: Yup.string().email('Invalid email').required('Email is Required.'),
+  });
+  return (
+    <Fragment>
+        <Container className='d-flex justify-content-center align-items-center'>
+          <ContentWrapper className='d-flex m-auto'>
+            <LeftSection className='d-flex flex-column align-items-center justify-content-center'>
+                <h3>Login</h3>
+                <LoginForm>
+                <Formik
+                  initialValues={{
+                      email: '',
+                      password: '',
+                  }}
+                  onSubmit={values => {
+                    console.log(values);
+                  }}
+                  validationSchema={LoginSchema}
+                >
+                  {({ errors, touched }) => (
+                  <Form className='form'>
+                    <Field 
+                      className={ `input-field ${errors.email && touched.email ? 'invalid' : null}` }
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder='Email' 
+                      />
+                     
+                    <Field
+                    className={ `input-field ${errors.password && touched.password ? 'invalid' : ''}` }
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder='Password' />
+                    <Field className="input-field" type="submit" value="Log in"/>
+                    {errors.email && touched.email ? (
+                        <div  className='error-msg'>{errors.email}</div>
+                      ) : null}
+                    {errors.password && touched.password ? (
+                        <div className='error-msg'>{errors.password}</div>
+                    ) : null}
+                  </Form>
+                  )}
+                  </Formik>
+                </LoginForm>
+            </LeftSection>
+            <RightSection className='d-flex flex-column align-items-center justify-content-center text-center text-white'>
+                <h3>Welcome Back Admin!</h3>
+            </RightSection>
+          </ContentWrapper>
+        </Container>
+    </Fragment>
+)
+}
--- src/pages/Login/StyledLogin.js
+++ src/pages/Login/StyledLogin.js
@@ -3,17 +3,84 @@ import styled from "styled-components";
 const Container = styled.div`
     height: 100vh;
     width: 100%;
-    background-color
+    background-color: var(--light-grey);
 `;
 
 const ContentWrapper = styled.div`
-
+    width: 800px;
+    background-color: #fff;
+    border-radius: 10px;
+    height: 60vh;
+    box-shadow: var(--b-shadow);
+    @media (max-width: 768px){
+        flex-direction: column-reverse;
+        height: 80%;
+        justify-content: center;
+        width: 95%;
+    }
 `;
 const LeftSection = styled.div`
+    width: 50%;
+    padding: 40px;
+    h3{
+        font-weight: 700;
+        text-transform: uppercase;
+    }
+    @media (max-width: 768px){  
+        width: 100%;
+        height: 50%;
+    }
 `;
 const RightSection = styled.div`
+    width: 50%;
+    background: var(--blue);
+    padding: 40px;
+    border-radius: 0px 10px 10px 0px;
+    h3{
+        text-transform: uppercase;
+        text-shadow: 3px 4px 7px rgba(81,67,21,0.8);
+        margin-bottom: 10px;
+        font-size: 35px;
+    }
+    @media (max-width: 768px){  
+        width: 100%;
+        height: 50%;
+        border-radius: 10px 10px 0px 0px;
+    }
 `;
-const LoginForm = styled.form`
+const LoginForm = styled.div`
+    width: 80%;
+
+    .input-field{
+        display: block;
+        border: 0px;
+        background-color: var(--light-grey);
+        outline: none;
+        margin: 10px 0px;
+        width: 100%;
+        height: 40px;
+        border-radius: 10px;
+        padding:  0px 15px;
+        position: relative;
+    }
+    .invalid{
+        background: #ff00003b;
+    }
+
+    .error-msg{
+        color: red;
+        font-weight: 600;
+        font-size: 12px;
+    }
+    .input-field[type="submit"]{
+        background-color: var(--blue);
+        color: white;
+        text-transform: uppercase;
+        transition: .3s;
+        :hover{
+        background: var(--hover-blue);
+        }
+    } 
 `;
 
 export {Container, ContentWrapper, LeftSection, RightSection, LoginForm}
