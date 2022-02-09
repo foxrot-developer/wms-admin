caaaab830c3e1258b080fd1783e732650eef0b32
RajaSabiq <ther.sabiqarfan@gmail.com>
2022-02-09 17:10:25 +0500

    Fixed Login

--- src/store/admin/actions/actionCreators.js
+++ src/store/admin/actions/actionCreators.js
@@ -51,5 +51,9 @@ function formatDateToString(date) {
 
   var MM = (date.getMonth() + 1 < 10 ? '0' : '') + (date.getMonth() + 1);
 
-  return `${date.getFullYear()}-${MM}-${dd} ${date.getUTCHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  return `${date.getFullYear()}-${MM}-${dd} ${
+    (date.getHours() < 10 ? '0' : '') + date.getHours()
+  }:${(date.getMinutes() < 10 ? '0' : '') + date.getMinutes()}:${
+    (date.getSeconds() < 10 ? '0' : '') + date.getSeconds()
+  }`;
 }
